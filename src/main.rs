b7048dbe20a3a41093a33c0290d72bfbd1fd811a
0xLogN <nutdriver716@gmail.com>
2023-05-08 14:41:21 -0700

    e

--- src/main.rs
+++ src/main.rs
@@ -321,24 +321,20 @@ impl<'a> InterpreterResolver<anyhow::Error> for Resolver<'a> {
 ///
 /// If the command is not found, it returns a code block containing the command name and prints
 /// a warning.
-async fn mention_application_command(ctx: &serenity::Context, command_string: &str) -> String {
-    let (command_name, _) = match command_string.split_once(' ') {
-        Some((command_name, subcommand_string)) => (command_name, Some(subcommand_string)),
-        None => (command_string, None),
-    };
+async fn mention_application_command(ctx: &serenity::Context, command_string: &str) -> Result<String, anyhow::Error> {
+    let command_name = command_string.split_once(' ').map(|(name, _)| name).unwrap_or(command_string);
 
-    match serenity::model::application::command::Command::get_global_application_commands(ctx).await
-    {
-        Ok(commands) => match commands.iter().find(|command| command.name == command_name) {
-            Some(command) => format!("</{}:{}>", command_string, command.id.0),
-            None => {
-                println!("WARN: Attempt to mention the command \"{}\" (root command {}) which was not found!", command_string, command_name);
-                format!("`/{}`", command_string)
-            }
-        },
-        Err(_) => {
-            println!("WARN: Error looking up global application commands!");
-            format!("`/{}`", command_string)
+    let command = serenity::model::application::command::Command::get_global_application_commands(ctx)
+        .await
+        .map_err(|_| anyhow::anyhow!("Error looking up global application commands!"))?
+        .into_iter()
+        .find(|command| command.name == command_name);
+
+    match command {
+        Some(command) => Ok(format!("</{}:{}>", command_string, command.id.0)),
+        None => {
+            println!("WARN: Attempt to mention the command \"{}\" (root command {}) which was not found!", command_string, command_name);
+            Ok(format!("`/{}`", command_string))
         }
     }
 }
