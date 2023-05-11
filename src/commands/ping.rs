caee2487c4629f9e24291249eb43b54fcc8cfd26
0xLogN <nutdriver716@gmail.com>
2023-05-11 09:37:40 -0700

    refactor ping command to avoid locking

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -15,24 +15,22 @@ pub async fn ping(ctx: Context<'_>) -> Result<(), anyhow::Error> {
         - new_ctx.interaction.id().created_at().timestamp_millis();
 
     // See https://github.com/serenity-rs/serenity/blob/6e2e70766e1afbce9cd1d4b43e3c6ee3b474f0bf/examples/e05_command_framework/src/main.rs#L468
-    // TODO: A lot of locking here. Could this block and be dangerous?
-    let shard_manager = ctx.data().shard_manager.lock().await;
-    let runners = shard_manager.runners.lock().await;
-    let current_shard = runners.get(&ShardId(ctx.serenity_context().shard_id));
+    let shard_latency = {
+        let shard_manager = ctx.data().shard_manager.lock().await;
+        let runners = shard_manager.runners.lock().await;
+        runners
+            .get(&ShardId(ctx.serenity_context().shard_id))
+            .ok_or(anyhow::anyhow!("Failed to obtain current shard"))?
+            .latency
+    };
 
     m.edit(ctx, |r| {
         r.content(format!(
-            "Pong :ping_pong:! (Round trip: {}ms. {})",
+            "Pong :ping_pong:! (Round trip: {}ms. Heartbeat: {}.)",
             diff_ms,
-            if let Some(info) = current_shard {
-                if let Some(latency) = info.latency {
-                    format!("Heartbeat: {}ms.", latency.as_millis())
-                } else {
-                    "No heartbeat available.".to_string()
-                }
-            } else {
-                "Failed to obtain current shard.".to_string()
-            }
+            shard_latency
+                .map(|l| format!("{}ms", l.as_millis()))
+                .unwrap_or_else(|| "unknown".to_string())
         ))
     })
     .await?;
