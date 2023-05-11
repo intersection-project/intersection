13bfa5e25603cb20c7677ad9edd1630c2f597700
0xLogN <nutdriver716@gmail.com>
2023-05-11 13:03:38 -0700

    no more error handling
    it was kinda useless

--- src/drql/parser.rs
+++ src/drql/parser.rs
@@ -1,47 +1,13 @@
-use std::fmt::Debug;
-use std::fmt::Display;
-
 use super::ast;
 use super::lexer;
 use crate::parser;
-use lalrpop_util::{ErrorRecovery, ParseError};
-
-#[derive(Debug, PartialEq)]
-pub enum DrqlParserError<T> {
-    /// An error we were able to recover from
-    Recoverable {
-        errors: Vec<ErrorRecovery<usize, lexer::Tok, lexer::LexicalError>>,
-        partial: T,
-    },
-    /// An error that stopped the parser.
-    Fatal(ParseError<usize, lexer::Tok, lexer::LexicalError>),
-}
-
-impl<T> Display for DrqlParserError<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DrqlParserError::Recoverable { errors, .. } => {
-                for error in errors {
-                    writeln!(f, "{}", error.error)?;
-                }
-                Ok(())
-            }
-            DrqlParserError::Fatal(e) => write!(f, "{}", e),
-        }
-    }
-}
-
-impl<T> std::error::Error for DrqlParserError<T> where T: Debug {}
+use lalrpop_util::ParseError;
 
 /// Parse a DRQL expression with the DRQL parser.
-pub fn parse_drql(input: &str) -> Result<ast::Expr, DrqlParserError<ast::Expr>> {
-    let mut errors = Vec::new();
-    let result = parser::ExprParser::new().parse(&mut errors, lexer::DrqlLexer::new(input));
-    match result {
-        Err(e) => Err(DrqlParserError::Fatal(e)),
-        Ok(v) if errors.is_empty() => Ok(v),
-        Ok(v) => Err(DrqlParserError::Recoverable { errors, partial: v }),
-    }
+pub fn parse_drql(
+    input: &str,
+) -> Result<ast::Expr, ParseError<usize, lexer::Tok, lexer::LexicalError>> {
+    parser::ExprParser::new().parse(lexer::DrqlLexer::new(input))
 }
 
 #[cfg(test)]
