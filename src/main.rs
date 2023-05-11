c72a4c45824876cef8e6fc8a07c29edca55c8938
0xLogN <nutdriver716@gmail.com>
2023-05-11 09:30:30 -0700

    move extension traits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod commands;
 mod drql;
+mod extensions;
 mod models;
 mod util;
 
@@ -15,12 +16,9 @@ lalrpop_mod!(
 use anyhow::{anyhow, bail};
 use dotenvy::dotenv;
 use drql::{ast, interpreter::InterpreterResolver};
+use extensions::{CustomGuildImpl, CustomMemberImpl, CustomRoleImpl};
 use poise::{async_trait, serenity_prelude as serenity};
-use std::{
-    collections::{HashMap, HashSet},
-    env,
-    sync::Arc,
-};
+use std::{collections::HashSet, env, sync::Arc};
 
 pub struct Data {
     /// The framework.shard_manager, used to get the latency of the current shard in the ping command
@@ -31,91 +29,6 @@ type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
 /// Function to fold an iterator of ASTs into one large union expression
 fn reduce_ast_chunks(iter: impl Iterator<Item = ast::Expr>) -> Option<ast::Expr> {
     iter.reduce(|acc, chunk| ast::Expr::Union(Box::new(acc), Box::new(chunk)))
-}
-
-trait CustomMemberImpl {
-    fn can_mention_role(
-        &self,
-        ctx: &serenity::Context,
-        role: &serenity::Role,
-    ) -> anyhow::Result<bool>;
-}
-impl CustomMemberImpl for serenity::Member {
-    fn can_mention_role(
-        &self,
-        ctx: &serenity::Context,
-        role: &serenity::Role,
-    ) -> anyhow::Result<bool> {
-        Ok(role.mentionable
-            || (self.permissions(ctx)?.mention_everyone())
-            || (self.permissions(ctx)?.administrator()))
-    }
-}
-
-trait CustomGuildImpl {
-    fn get_everyone(&self) -> HashSet<serenity::UserId>;
-    fn get_here(&self) -> HashSet<serenity::UserId>;
-    fn all_roles_and_members(
-        &self,
-        ctx: &serenity::Context,
-    ) -> anyhow::Result<HashMap<models::mention::RoleType, HashSet<serenity::UserId>>>;
-}
-impl CustomGuildImpl for serenity::Guild {
-    fn get_everyone(&self) -> HashSet<serenity::UserId> {
-        self.members
-            .values()
-            .map(|member| member.user.id)
-            .collect::<HashSet<_>>()
-    }
-    fn get_here(&self) -> HashSet<serenity::UserId> {
-        self.get_everyone()
-            .into_iter()
-            .filter(|id| {
-                if let Some(presence) = self.presences.get(id) {
-                    presence.status != serenity::OnlineStatus::Offline
-                } else {
-                    false
-                }
-            })
-            .collect::<HashSet<_>>()
-    }
-    fn all_roles_and_members(
-        &self,
-        ctx: &serenity::Context,
-    ) -> anyhow::Result<HashMap<models::mention::RoleType, HashSet<serenity::UserId>>> {
-        let mut map = HashMap::from([
-            (models::mention::RoleType::Everyone, self.get_everyone()),
-            (models::mention::RoleType::Here, self.get_here()),
-        ]);
-
-        for member in self.members.values() {
-            for role in member.roles(ctx).ok_or(anyhow!(
-                "Failed to get user role data for {}",
-                member.user.id
-            ))? {
-                map.entry(models::mention::RoleType::Role(role.id))
-                    .or_insert_with(HashSet::new)
-                    .insert(member.user.id);
-            }
-        }
-
-        Ok(map)
-    }
-}
-
-trait CustomRoleImpl {
-    fn members(&self, guild: &serenity::Guild) -> HashSet<serenity::UserId>;
-}
-impl CustomRoleImpl for serenity::Role {
-    fn members(&self, guild: &serenity::Guild) -> HashSet<serenity::UserId> {
-        HashSet::from_iter(
-            guild
-                .members
-                .values()
-                .filter(|member| member.roles.contains(&self.id))
-                .map(|member| member.user.id),
-        )
-    }
 }
 
 /// The custom instance of the DRQL [InterpreterResolver] used for Intersection.
