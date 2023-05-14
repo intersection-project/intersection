d74e3569aecc9de26d407c436f7dd3079459427f
0xLogN <nutdriver716@gmail.com>
2023-05-14 12:28:19 -0700

    e

--- build.rs
+++ build.rs
@@ -11,4 +11,8 @@ fn main() {
     )
     .expect("Failed to acquire build-time information");
     lalrpop::process_root().unwrap();
+    println!("cargo:rerun-if-changed=src/grammar.lalrpop");
+    println!("cargo:rerun-if-changed=.git"); // because of git sha in build data
+    println!("cargo:rerun-if-changed=Cargo.lock"); // similar
+    println!("cargo:rerun-if-changed=Cargo.toml"); // ditto
 }
