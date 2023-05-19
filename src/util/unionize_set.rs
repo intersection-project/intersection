ea8b9310ba5d9e7367c649e9024fe6c42fa254e9
0xLogN <nutdriver716@gmail.com>
2023-05-19 12:32:42 -0700

    unionize_set v2?

--- src/util/unionize_set.rs
+++ src/util/unionize_set.rs
@@ -36,6 +36,10 @@ where
 /// function may change without notice.
 ///
 /// TODO: Example
+/// TODO: Is this ALWAYS correct? Prove it!
+///
+/// RFC in #16
+/// PR in #18
 pub fn unionize_set<'a, Key, Value>(
     target: &'a HashSet<Value>,
     preexisting_sets: &'a HashMap<Key, HashSet<Value>>,
@@ -44,51 +48,127 @@ where
     Key: PartialEq + Eq + Hash + Copy,
     Value: PartialEq + Eq + Hash + Copy,
 {
-    // First, we will determine which sets in preexisting_sets even *qualify* to be used in the output.
-    // The condition here is simple: A qualifying set must be a subset of the target.
-    let qualifying_sets = preexisting_sets
+    // A clone of every one of the preexisting sets, excluding those that aren't subsets of target
+    let mut cloned_sets = preexisting_sets
         .iter()
         .filter(|(_, set)| set.is_subset(target))
+        .map(|(k, v)| (*k, v.clone()))
         .collect::<HashMap<_, _>>();
 
-    // Next, we remove so-called "redundant qualifiers." A qualifier is considered redundant if
-    // another qualifying set is a superset of it -- because if we take the union, all of the values
-    // in this set will be included in one of the other sets, making this set unnecessary.
-    let optimized_qualifiers = qualifying_sets
-        .iter()
-        .filter(|(key, value)| {
-            // Filter out any values where this predicate is true for any value within qualifying_sets...
-            !(qualifying_sets.iter().any(|(other_key, other_value)| {
-                // ...except for the value itself.
-                // FIXME: In the case that two qualifiers have identical member lists,
-                //        (e.g.: @{everyone} where @everyone are all online so @everyone==@here),
-                //        this will remove *both* sets. this is not catastrophic, as optimization
-                //        is sorta "best-effort" and the outliers will be caught, but it's nice
-                //        if we can fix this
-                *key != other_key && value.is_subset(other_value)
-            }))
-        })
-        .map(|(&a, &b)| (a, b))
-        .collect::<HashMap<_, _>>();
+    let mut cloned_target = target.clone();
 
-    // Next, we calculate the union of all of the sets we qualified! This is used in calculating
-    // outliers, later.
-    let union_of_qualifiers = optimized_qualifiers
-        .values()
-        .copied() // TODO: remove copy
-        .flatten()
-        .collect::<HashSet<_>>();
+    let mut output_keys: HashSet<Key> = HashSet::new();
 
-    let outliers = target
-        .iter()
-        .collect::<HashSet<_>>()
-        .difference(&union_of_qualifiers)
-        .copied() // TODO: remove copy
-        .collect::<HashSet<_>>();
+    while cloned_sets.values().any(|set| set.len() > 0) {
+        let max_size = cloned_sets
+            .iter()
+            .map(|(_, set)| set.len())
+            .max()
+            .expect("cloned_sets is empty"); // This should never happen, as the .any() call would fail
 
+        let sets_with_max_size = cloned_sets
+            .iter()
+            .filter(|(_, set)| set.len() == max_size)
+            .collect::<Vec<_>>();
+
+        // Depending on how many sets we have...
+        // All of the sets in sets_with_max_size have the same length.
+        // We must pick ONE set to work with. There is a few possible cases for the interaction between
+        // two sets:
+        // - EQUALITY: Both sets are exactly equal (a == b)
+        // - OVERLAP: Some items are shared between each set (cardinality(a intersection b) != 0)
+        // - DISTINCTION: No items are shared between each set (card(a intersection b) == 0)
+        // If we can find at least one DISTINCT set from all of the "conflicting" sets (those in
+        // sets_with_max_size) then we can use any of those sets. If there is no distinct set, then
+        // we will select whichever set has the most unique elements in it (the set with the least
+        // overlapping elements). If there is a NON-ZERO tie, any selection works. If all sets unique counts are 0,
+        // we can choose any set because they must all be equal.
+        let selected_set = match sets_with_max_size.len() {
+            // REACHABILITY: This is unreachable as the .max() call would return None
+            0 => unreachable!(),
+            1 => {
+                // There is no conflict, we can use this set!
+                // this cannot panic as len>1 implies that there is at least one element
+                sets_with_max_size[0]
+            }
+            _ => {
+                // A set is distinct if it is not a subset of the union of all other sets within
+                // sets_with_max_size.
+
+                // Get the union of all sets within sets_with_max_size except for a specific index
+                let union_of_all_sets_except = |index: usize| {
+                    sets_with_max_size
+                        .iter()
+                        .enumerate()
+                        .filter(|(i, _)| *i != index)
+                        .map(|(_, (_, set))| set)
+                        .copied()
+                        .flatten()
+                        .collect::<HashSet<_>>()
+                };
+
+                let is_distinct = |index: usize| {
+                    !sets_with_max_size[index]
+                        .1
+                        .iter()
+                        .collect::<HashSet<_>>()
+                        .is_subset(&union_of_all_sets_except(index))
+                };
+
+                // Find the first distinct set, if any
+                let first_distinct_set = sets_with_max_size
+                    .iter()
+                    .enumerate()
+                    .find(|(i, _)| is_distinct(*i));
+
+                match first_distinct_set {
+                    // If there is a distinct set, use it
+                    Some((i, _)) => sets_with_max_size[i],
+                    // Otherwise, use the set with the most unique elements
+                    None => *sets_with_max_size
+                        .iter()
+                        .max_by_key(|(_, set)| {
+                            set.iter()
+                                .filter(|&x| !union_of_all_sets_except(0).contains(x))
+                                .count()
+                        })
+                        .expect("sets_with_max_size is empty"), // This should never happen, as the .any() call would fail
+                }
+            }
+        };
+
+        // selected_set is now the set we'd like to use. we can add it to our output:
+        output_keys.insert(selected_set.0.clone());
+
+        let cloned = selected_set.1.clone();
+
+        // Remove all of the values in selected_set.1 from every set in cloned sets
+        cloned_sets = cloned_sets
+            .into_iter()
+            .map(|(k, set)| (k, set.difference(&cloned).copied().collect()))
+            .collect();
+        cloned_target = cloned_target.difference(&cloned).copied().collect();
+    }
+
+    // Outliers = cloned_target
+    // Output sets = output_keys
     UnionizeSetResult {
-        sets: optimized_qualifiers.into_keys().collect::<HashSet<_>>(),
-        outliers,
+        // Map each key back to its original in preexisting_sets - this is just to convert from
+        // Key to &Key without returning data owned by this fn
+        sets: output_keys
+            .into_iter()
+            .map(|key| preexisting_sets.get_key_value(&key).unwrap().0)
+            .collect(),
+        // Ditto
+        outliers: cloned_target
+            .into_iter()
+            .map(|value| {
+                target
+                    .iter()
+                    .find(|&x| *x == value)
+                    .expect("value not in target")
+            })
+            .collect(),
     }
 }
 
