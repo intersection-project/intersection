f5bcf6d11ced121536a5b05bc022a4f249f951aa
0xLogN <logn@0xlogn.dev>
2024-02-08 08:55:56 -0800

    fix: fixes for breaking changes in built 0.7.1

--- build.rs
+++ build.rs
@@ -1,15 +1,5 @@
 fn main() {
-    built::write_built_file_with_opts(
-        built::Options::default().set_dependencies(true),
-        std::env::var("CARGO_MANIFEST_DIR")
-            .expect("Expected CARGO_MANIFEST_DIR in the environment")
-            .as_ref(),
-        &std::path::Path::new(
-            &std::env::var("OUT_DIR").expect("Expected OUT_DIR in the environment"),
-        )
-        .join("built.rs"),
-    )
-    .expect("Failed to acquire build-time information");
+    built::write_built_file().expect("Failed to acquire build-time information");
     lalrpop::process_root().unwrap();
     println!("cargo:rerun-if-changed=src/grammar.lalrpop");
     println!("cargo:rerun-if-changed=.git"); // because of git sha in build data
